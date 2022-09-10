6768917e7e51b7fc46679ada001c498f6ac5d2ff
oscar_gomez <oscar.gomez@ecole2600.com>
2022-09-10 12:07:44 +0200

    update exit code

--- dns_recon.py
+++ dns_recon.py
@@ -7,17 +7,12 @@ import sys
 
 if len(sys.argv) < 2:
     print('Usage: dns_recon target.com')
-    exit(128)
+    exit(11)
 else:
     target=sys.argv[1]
 
 
 dnsIP='8.8.8.8'
-
-try:
-    answers = dns.resolver.resolve(target, 'A')
-except:
-    dnsIP='1.1.1.1'
 
 my_resolver = dns.resolver.Resolver()
 my_resolver.nameservers = [dnsIP]
@@ -25,10 +20,24 @@ my_resolver.nameservers = [dnsIP]
 def main():
     print(f'Starting dump on: {target}')
     print('-' * 20)
-    full_scan(['A', 'TXT', 'MX', 'CNAME', 'AAAA'])
+    records_scan(['A', 'TXT', 'MX', 'CNAME', 'AAAA'])
     print('-' * 20)
 
-def full_scan(queries):
+def records_scan(queries):
+    print(f'- DNS Server: {dnsIP}')
+    print('\n')
+
+    for query in queries:
+        try:
+            result = dns.resolver.resolve(target, query)
+            for exdata in result:
+                print(f'- {query} Record:', exdata)
+        except:
+            print(f'- No {query} Records')
+
+        print('\n')
+
+def records_scan(queries):
     print(f'- DNS Server: {dnsIP}')
     print('\n')
 
