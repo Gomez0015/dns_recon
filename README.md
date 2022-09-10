6ac96fe955250775cbb2ce595dfa4529aaef883f
oscar_gomez <oscar.gomez@ecole2600.com>
2022-09-10 12:50:40 +0200

    Subdomain Enumeration, Custom Keyboard Interupt, Reverse IP Lookup

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Usage: dns_recon target.com
 ## Global Command:
 - chmod +x ./dns_recon.py
 - sudo cp ./dns_recon.py /usr/bin/dns_recon
+- sudo cp ./dicts /usr/share/dns_recon/
--- dns_recon.py
+++ dns_recon.py
@@ -4,13 +4,17 @@
 
 import dns.resolver
 import sys
+import time
+import socket
 
 if len(sys.argv) < 2:
     print('Usage: dns_recon target.com')
     exit(11)
 else:
-    target=sys.argv[1]
-
+    try:
+        target=socket.gethostbyaddr(sys.argv[1])[0]
+    except socket.herror:
+        target=sys.argv[1]
 
 dnsIP='8.8.8.8'
 
@@ -22,6 +26,8 @@ def main():
     print('-' * 20)
     records_scan(['A', 'TXT', 'MX', 'CNAME', 'AAAA'])
     print('-' * 20)
+    enum_subdomains()
+    print('-' * 20)
 
 def records_scan(queries):
     print(f'- DNS Server: {dnsIP}')
@@ -37,18 +43,41 @@ def records_scan(queries):
 
         print('\n')
 
-def records_scan(queries):
-    print(f'- DNS Server: {dnsIP}')
-    print('\n')
+LINE_UP = '\033[1A'
+LINE_CLEAR = '\x1b[2K'
 
-    for query in queries:
-        try:
-            result = dns.resolver.resolve(target, query)
-            for exdata in result:
-                print(f'- {query} Record:', exdata)
-        except:
-            print(f'- No {query} Records')
+def enum_subdomains():
+    try:
+        wordlist = open("./dicts/subdomains.txt", "r")
+    except:
+        wordlist = open("/usr/share/dns_recon/dicts/subdomains.txt", "r")
 
-        print('\n')
+    queries = ['A', 'TXT']
+    total = wordlist.readline().replace('\n', '')
+    index = 0
 
-main()
+    print("Starting subdomains search")
+
+    for word in wordlist:
+        found = False
+
+        word = word.replace('\n', '')
+        print(f"Trying {index}/{total} {word}")
+        print(LINE_UP, end=LINE_CLEAR)
+
+        for query in queries:
+            try:
+                dns.resolver.resolve(f'{word}.{target}', query)
+                found = True
+            except Exception:
+                pass
+
+        if found:
+            print(f'- {word}.{target}')
+
+        index += 1
+
+try:
+    main()
+except KeyboardInterrupt:
+    print('\nKeyboard Interrupt /!\ \n')
